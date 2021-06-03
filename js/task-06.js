f53b4c28beafc4e9390848e286f9d6ebf1773ca0
Ivanna Dvorianska <idvoryanska@gmail.com>
2021-06-03 18:19:30 +0200

    module 7 task 6 finished

---
+++ js/task-06.js
@@ -0,0 +1,18 @@
+const inputRef = document.querySelector("#validation-input");
+const inputAttribute = inputRef.getAttribute("data-length");
+
+inputRef.addEventListener("blur", verifyValidationOfInput);
+
+function verifyValidationOfInput(event) {
+  const attributeValue = Number(inputAttribute);
+
+  if (event.currentTarget.value.length === attributeValue) {
+    event.currentTarget.classList.add("valid");
+    event.currentTarget.classList.remove("invalid");
+  }
+
+  if (event.currentTarget.value.length !== attributeValue) {
+    event.currentTarget.classList.add("invalid");
+    event.currentTarget.classList.remove("valid");
+  }
+}
